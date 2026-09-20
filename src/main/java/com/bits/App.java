87fd5ed5dba1a09bc4e82a6a06769a8e7d865875
amar2022wa86495 <2022wa86495@wilp.bits-pilani.ac.in>
2026-09-20 21:12:12 +0530

    Update message to version 2

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,7 +2,7 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps - Version 1");
+        System.out.println("Hello DevOps - Version 2 (CI triggered)");
     }
     public static int add(int a, int b) { return a + b; }
 }
