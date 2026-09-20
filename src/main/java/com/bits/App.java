2c462191863ca376c6eeb193967d94a97008062a
AMAR <2022wa86495@wilp.bits-pilani.ac.in>
2026-09-20 20:26:51 +0530

    first commit

---
+++ src/main/java/com/bits/App.java
@@ -0,0 +1,8 @@
+package com.bits;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps - Version 1");
+    }
+    public static int add(int a, int b) { return a + b; }
+}
